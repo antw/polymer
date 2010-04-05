5644ce49fb9db1cfd91d3a5d7103fd2ef6cdf2f9
Anthony Williams <hi@antw.me>
2010-04-05 22:53:02 +0100

    Generator no longer regenerated unchanged sprites.

--- lib/montage/commands/generate.rb
+++ lib/montage/commands/generate.rb
@@ -2,20 +2,17 @@ module Montage
   module Commands
     # Generates sprites for a project.
     class Generate
-      extend Commands
-      extend Term::ANSIColor
+      extend  Commands
+      extend  Term::ANSIColor
+      include Term::ANSIColor
 
       # Given a project, generates sprites.
       #
       # @param [Array] argv
       #   The arguments given on the command line.
       #
-      def self.run(argv)
-        Montage::Project.find(Dir.pwd).sprites.each do |sprite|
-          $stdout.print "#{sprite.name}: "
-          sprite.write
-          $stdout.puts green("Generated")
-        end
+      def self.run(*)
+        new(Montage::Project.find(Dir.pwd)).run!
 
       rescue Montage::MissingProject
         $stdout.puts red(<<-ERROR)
@@ -31,6 +28,73 @@ module Montage
         $stdout.puts red(e.message)
 
         exit(1)
+      end
+
+      # Creates a new Generate instance.
+      #
+      # @param [Montage::Project]
+      #   The project whose sprites are to be generated.
+      #
+      def initialize(project)
+        @project = project
+      end
+
+      # Runs the generator, saving the sprites and cache.
+      #
+      def run!
+        generate_sprites!
+        write_cache!
+      end
+
+      private # ==============================================================
+
+      # Returns the cached digests.
+      #
+      # @return [Hash]
+      #
+      def cache
+        @_sprite_caches ||= begin
+          cache_path = @project.paths.sprites + '.montage_cache'
+          cache_path.file? ? YAML.load_file(cache_path) || {} : {}
+        end
+      end
+
+      # Generates the sprites for the given project. Skips those which have
+      # not changed since they were last generated.
+      #
+      # @param [Montage::Project] project
+      #   The project whose sprites are to be generated.
+      # @param [Hash] cache
+      #   The cached digests for the project.
+      #
+      def generate_sprites!
+        @project.sprites.each do |sprite|
+          digest = sprite.digest
+          $stdout.print "#{sprite.name}: "
+
+          if cache[sprite.name] == digest
+            $stdout.puts yellow("Unchanged: ignoring")
+          else
+            sprite.write
+            cache[sprite.name] = digest
+            $stdout.puts green("Generated")
+          end
+        end
+      end
+
+      # Writes the cached digests to the cache file.
+      #
+      # @param [Pathname] montage_cache
+      #   Path to the cache file.
+      # @param [Hash] cache
+      #   A hash of sprite names and their calculated digests.
+      #
+      def write_cache!
+        cache_path = @project.paths.sprites + '.montage_cache'
+
+        File.open(cache_path, 'w') do |cache_writer|
+          cache_writer.puts YAML.dump(cache)
+        end
       end
 
     end # Generate
--- spec/montage/commands/generate_spec.rb
+++ spec/montage/commands/generate_spec.rb
@@ -108,7 +108,7 @@ end
 
 # ----------------------------------------------------------------------------
 
-context 'Trying to generate sprites when the source directory' do
+context 'Trying to generate sprites when the source directory does not exist' do
   before(:all) do
     @runner = Montage::Spec::CommandRunner.new('montage')
     @runner.write_config <<-CONFIG
@@ -120,5 +120,40 @@ context 'Trying to generate sprites when the source directory' do
   end
 
   it { @runner.should be_failure }
-  it { @runner.stdout.should =~ /Montage can't save the sprite in/ }
+  it { @runner.stdout.should =~ /Couldn't find the source directory/ }
+end
+
+# ----------------------------------------------------------------------------
+
+context 'Generating two sprites, one of which is unchanged' do
+  before(:all) do
+    @runner = Montage::Spec::CommandRunner.new('montage')
+    @runner.write_config <<-CONFIG
+      ---
+        sprite_one:
+          - one
+          - two
+
+        sprite_two:
+          - three
+    CONFIG
+    @runner.write_source('one')
+    @runner.write_source('two')
+    @runner.write_source('three')
+    @runner.run!
+
+    # Change the 'one' source.
+    @runner.write_source('one', 100, 25)
+    @runner.run!
+  end
+
+  it { @runner.should be_success }
+
+  it { @runner.stdout.should =~ /sprite_one: Generated/ }
+  it { @runner.path_to_sprite('sprite_one').should be_file }
+  it { @runner.dimensions_of('sprite_one').should == [100, 65] }
+
+  it { @runner.stdout.should =~ /sprite_two: Unchanged/ }
+  it { @runner.path_to_sprite('sprite_two').should be_file }
+  it { @runner.dimensions_of('sprite_two').should == [50, 20] }
 end
